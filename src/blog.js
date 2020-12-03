bd7ed66a15b04ab43746ee50397c55afc0fdab0a
Ajin Varghese <ajin.varghese@contentstack.com>
2020-12-04 00:18:13 +0530

    modified all files
    Refracted all code from synchronize to asyncand modified data file such that it can now  fetch data through api call async using axios

--- src/blog.js
+++ src/blog.js
@@ -1,26 +1,42 @@
 import { data } from "./data.js";
 const root = document.querySelector(".screen-to-render");
+var num;
+var result;
 
 //For rendering block content
-export const render_blog = (x) => {
-  document.querySelector(".screen-to-render").innerHTML = `<h1>${x.title}</h1>
-<img src="${x.imageUrl}" class="banner-image" width="80%;" />
-<p class="blog-content">${x.content}</p>`;
-  render_aside(x);
+export let render_blog = (res) => {
+  if (res.length > 1) {
+    result = res;
+    num = res[Math.floor(Math.random() * (res.length-1) - 0 + 1) + 0];
+    document.querySelector(
+      ".screen-to-render"
+    ).innerHTML = `<h1>${num.title}</h1>
+    <img src="${num.imageUrl}" class="banner-image" width="80%; alt="Blog_image" />
+    <p class="blog-content">${num.content}</p>`;
+    render_aside(num);
+  }
+  else{
+    document.querySelector(
+      ".screen-to-render"
+    ).innerHTML = `<h1>${res[0].title}</h1>
+    <img src="${res[0].imageUrl}" class="banner-image" width="80%; alt="Blog_image" />
+    <p class="blog-content">${res[0].content}</p>`;
+    render_aside(res[0]);
+  }
 };
 
 //For rendering related links ka section.
 function render_aside(blog) {
   let aside = document.querySelector("aside");
   aside.innerHTML = `<h1 id="aside-header"> Related Links </h1>`;
-  let ul = document.createElement('ul');
-  ul.className="aside-list-item";
+  let ul = document.createElement("ul");
+  ul.className = "aside-list-item";
   aside.appendChild(ul);
   blog.links.forEach((link) => {
-    let list = document.createElement('li');
-    list.id =link.id;
+    let list = document.createElement("li");
+    list.id = link.id;
     list.innerHTML = link.title;
-    list.onclick =()=>{
+    list.onclick = () => {
       freshBlog(event);
     };
     ul.appendChild(list);
@@ -31,14 +47,19 @@ function render_aside(blog) {
   });
 }
 
-
-//EVENT LISTENER KA PART 
+//EVENT LISTENER KA PART
 function freshBlog(event) {
   document.querySelector(".screen-to-render").innerHTML = "";
   document.querySelector("aside").innerHTML = "";
 
+  function to_render(i) {
+    return i.id == event.target.id;
+  }
+  render_blog(result.filter(to_render));
+  // console.log(result[0].id == "2rvqpdbpka3n3fhd");
+
   //render the blog with specified id
-  let data_to_fetch= data.filter((i)=>
-    i.id === event.target.id);
- render_blog(data_to_fetch[0]); 
+  //   let data_to_fetch= result.filter((i)=>
+  //     i.id === event.target.id);
+  //  render_blog(data_to_fetch[0]);
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { render_blog} from "./blog.js";
 import {data} from "./data.js";
 
-render_blog(data[Math.floor(Math.random()* data.length)]);
-// freshBlog(data);
+
+// data.then(r=>{
+// render_blog(r[Math.floor(Math.random() * data.length)]);
+// });
